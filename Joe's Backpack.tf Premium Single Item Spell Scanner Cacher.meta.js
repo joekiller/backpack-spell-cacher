7a7866de390d3eef2209eea537d790fdf0cee2ae
Joseph Lawson <joe@joekiller.com>
2022-01-16 00:12:17 -0500

    add meta

---
+++ Joe's Backpack.tf Premium Single Item Spell Scanner Cacher.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Joe's Backpack.tf Premium Single Item Spell Scanner/Cacher
+// @namespace    https://joekiller.com
+// @version      1.0
+// @description  Scans through backpack.tf premium search pages with ctrl+leftarrow. The scanning stops when all things, crafable and quality types are exhausted. Ctrl + down arrow to force stop.
+// @author       Joseph Lawson
+// @match        *backpack.tf/premium/search*
+// ==/UserScript==
